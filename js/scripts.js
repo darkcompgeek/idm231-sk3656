1db6b7b16d842b2d04a53ef8c7a08ae702a0121b
Sai Kannekanti <darkcompgeek@gmail.com>
2021-03-20 01:24:19 -0400

    added the basic zodiac functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,13 @@
 /*
 VARIABLES FOR MODAL
 */
+const _modalHeader = document.getElementById("modal-header");
+//handle for modal header
+const _modalHeaderText = document.getElementById("modal-header-text");
+//handle for modal date range
+const _modalDateRangeText = document.getElementById("modal-dateRange-text");
+//handle for modal description
+const _modalDescriptionText = document.getElementById("modal-Description-text");
 
 //handle for the modal itself
 const _modal = document.getElementById("modal");
@@ -8,8 +15,12 @@ const _modal = document.getElementById("modal");
 const _closeModalButton = document.getElementById("button-closeModal");
 
 /*
-VARIABLES FOR FORM BUTTONS
+VARIABLES FOR FORM ELEMENTS
 */
+// //handle to first name field
+// const firstNameField = document.getElementById("input-name");
+// //handle to date field
+// const dateField = document.getElementById("input-date");
 
 //handle to submit button
 const _submitButton = document.getElementById("button-submit");
@@ -52,18 +63,254 @@ function ShowModal() {
   _modal.hidden = !_modal.hidden;
 }
 
+function CalculateZodiac(whichOne) {
+  console.log("userPicked called " + whichOne);
+  switch (whichOne) {
+    case "aqu":
+      // display title
+      _modalHeaderText.innerHTML = "District Line";
+      // display date range
+      _modalDateRangeText.innerHTML = "##-Month to ##-Month";
+      // display description
+      _modalDescriptionText.innerHTML = "<p>Aquarius are .....</p>";
+
+      _modalHeader.style.borderColor = "#00843d";
+      // Show hide modal
+      ShowModal();
+      break;
+    case "pis":
+      // display title
+      _modalHeaderText.innerHTML = "Bakerloo Line";
+      // display date range
+      _modalDateRangeText.innerHTML = "##-Month to ##-Month";
+      // display description
+      _modalDescriptionText.innerHTML =
+        "<p>Pisces are cool, they swim in water.</p>";
+
+      _modalHeader.style.borderColor = "#b0600f";
+      // Show hide modal
+      ShowModal();
+      break;
+    case "ari":
+      // display title
+      _modalHeaderText.innerHTML = "Central Line";
+      // display date range
+      _modalDateRangeText.innerHTML = "##-Month to ##-Month";
+      // display description
+      _modalDescriptionText.innerHTML = "<p>Aquarius are .....</p>";
+
+      _modalHeader.style.borderColor = "#ee2e22";
+      // Show hide modal
+      ShowModal();
+      break;
+    case "tau":
+      // display title
+      _modalHeaderText.innerHTML = "Circle Line";
+      // display date range
+      _modalDateRangeText.innerHTML = "##-Month to ##-Month";
+      // display description
+      _modalDescriptionText.innerHTML = "<p>Aquarius are .....</p>";
+
+      _modalHeader.style.borderColor = "#ffd104";
+      // Show hide modal
+      ShowModal();
+      break;
+    case "gem":
+      // display title
+      _modalHeaderText.innerHTML = "Hammersmith & City Line";
+      // display date range
+      _modalDateRangeText.innerHTML = "##-Month to ##-Month";
+      // display description
+      _modalDescriptionText.innerHTML = "<p>Aquarius are .....</p>";
+
+      _modalHeader.style.borderColor = "#f386a0";
+      // Show hide modal
+      ShowModal();
+      break;
+    case "can":
+      // display title
+      _modalHeaderText.innerHTML = "Jubilee Line";
+      // display date range
+      _modalDateRangeText.innerHTML = "##-Month to ##-Month";
+      // display description
+      _modalDescriptionText.innerHTML = "<p>Aquarius are .....</p>";
+
+      _modalHeader.style.borderColor = "#959ca1";
+      // Show hide modal
+      ShowModal();
+      break;
+    case "leo":
+      // display title
+      _modalHeaderText.innerHTML = "Metropolitan Line";
+      // display date range
+      _modalDateRangeText.innerHTML = "##-Month to ##-Month";
+      // display description
+      _modalDescriptionText.innerHTML = "<p>Aquarius are .....</p>";
+
+      _modalHeader.style.borderColor = "#97025f";
+      // Show hide modal
+      ShowModal();
+      break;
+    case "vir":
+      // display title
+      _modalHeaderText.innerHTML = "Northern Line";
+      // display date range
+      _modalDateRangeText.innerHTML = "##-Month to ##-Month";
+      // display description
+      _modalDescriptionText.innerHTML = "<p>Scorpio are .....</p>";
+
+      _modalHeader.style.borderColor = "#231f20";
+      // Show hide modal
+      ShowModal();
+      break;
+    case "lib":
+      // display title
+      _modalHeaderText.innerHTML = "Piccadilly Line";
+      // display date range
+      _modalDateRangeText.innerHTML = "##-Month to ##-Month";
+      // display description
+      _modalDescriptionText.innerHTML = "<p>Aquarius are .....</p>";
+
+      _modalHeader.style.borderColor = "#1b3f94";
+      // Show hide modal
+      ShowModal();
+      break;
+    case "sco":
+      // display title
+      _modalHeaderText.innerHTML = "Victoria Line";
+      // display date range
+      _modalDateRangeText.innerHTML = "##-Month to ##-Month";
+      // display description
+      _modalDescriptionText.innerHTML = "<p>Aquarius are .....</p>";
+
+      _modalHeader.style.borderColor = "#069ddc";
+      // Show hide modal
+      ShowModal();
+      break;
+    case "sag":
+      // display title
+      _modalHeaderText.innerHTML = "Waterloo & City Line";
+      // display date range
+      _modalDateRangeText.innerHTML = "##-Month to ##-Month";
+      // display description
+      _modalDescriptionText.innerHTML = "<p>Aquarius are .....</p>";
+
+      _modalHeader.style.borderColor = "#84cdbc";
+      // Show hide modal
+      ShowModal();
+      break;
+    case "cap":
+      // display title
+      _modalHeaderText.innerHTML = "Elizabeth Line";
+      // display date range
+      _modalDateRangeText.innerHTML = "##-Month to ##-Month";
+      // display description
+      _modalDescriptionText.innerHTML = "<p>Aquarius are .....</p>";
+
+      _modalHeader.style.borderColor = "#753dbd";
+      // Show hide modal
+      ShowModal();
+      break;
+    default:
+      console.log("default value tripped");
+  }
+}
+
 /*
-EVENT LISTENERS FOR MODAL BUTTONS
-*/
+                            EVENT LISTENERS FOR MODAL BUTTONS
+                            */
 
 _closeModalButton.addEventListener("click", ShowModal);
 
 /*
-EVENT LISTENERS FOR FORM BUTTONS
-*/
+                           EVENT LISTENERS FOR FORM BUTTONS
+                           */
 
 //click event listener for submit button
 _submitButton.addEventListener("click", function () {
+  // Get the user first name and store it in a variable
+  const firstName = document.getElementById("input-name").value;
+  console.log("userFirstName is: " + firstName);
+
+  // Get the user date and extract the month of year and day of month
+  const userBday = new Date(document.getElementById("input-date").value);
+  console.log("userBday is: " + userBday);
+
+  // Figure out the zodiac sign based on month and of year and day of month
+  let whichMonth = userBday.getUTCMonth();
+  whichMonth++;
+  console.log("whichMonth is: " + whichMonth);
+
+  const whichDayOfMonth = userBday.getUTCDate();
+  console.log("whichDayOfMonth is: " + whichDayOfMonth);
+
+  let zodiacSign = "";
+
+  if (
+    (whichMonth == 12 && whichDayOfMonth >= 22) ||
+    (whichMonth == 1 && whichDayOfMonth <= 19)
+  ) {
+    zodiacSign = "cap";
+  } else if (
+    (whichMonth == 11 && whichDayOfMonth >= 22) ||
+    (whichMonth == 12 && whichDayOfMonth <= 21)
+  ) {
+    zodiacSign = "sag";
+  } else if (
+    (whichMonth == 10 && whichDayOfMonth >= 24) ||
+    (whichMonth == 11 && whichDayOfMonth <= 21)
+  ) {
+    zodiacSign = "sco";
+  } else if (
+    (whichMonth == 9 && whichDayOfMonth >= 23) ||
+    (whichMonth == 10 && whichDayOfMonth <= 23)
+  ) {
+    zodiacSign = "lib";
+  } else if (
+    (whichMonth == 8 && whichDayOfMonth >= 23) ||
+    (whichMonth == 9 && whichDayOfMonth <= 22)
+  ) {
+    zodiacSign = "vir";
+  } else if (
+    (whichMonth == 7 && whichDayOfMonth >= 23) ||
+    (whichMonth == 8 && whichDayOfMonth <= 22)
+  ) {
+    zodiacSign = "leo";
+  } else if (
+    (whichMonth == 6 && whichDayOfMonth >= 22) ||
+    (whichMonth == 7 && whichDayOfMonth <= 22)
+  ) {
+    zodiacSign = "can";
+  } else if (
+    (whichMonth == 5 && whichDayOfMonth >= 21) ||
+    (whichMonth == 6 && whichDayOfMonth <= 21)
+  ) {
+    zodiacSign = "gem";
+  } else if (
+    (whichMonth == 4 && whichDayOfMonth >= 20) ||
+    (whichMonth == 5 && whichDayOfMonth <= 20)
+  ) {
+    zodiacSign = "tau";
+  } else if (
+    (whichMonth == 3 && whichDayOfMonth >= 21) ||
+    (whichMonth == 4 && whichDayOfMonth <= 19)
+  ) {
+    zodiacSign = "ari";
+  } else if (
+    (whichMonth == 2 && whichDayOfMonth >= 19) ||
+    (whichMonth == 3 && whichDayOfMonth <= 20)
+  ) {
+    zodiacSign = "pis";
+  } else if (
+    (whichMonth == 1 && whichDayOfMonth >= 20) ||
+    (whichMonth == 2 && whichDayOfMonth <= 18)
+  ) {
+    zodiacSign = "aqu";
+  }
+
+  console.log("AstroSign is: " + zodiacSign);
+
+  CalculateZodiac(zodiacSign);
   ShowModal();
 });
 //click event listener for help button
@@ -77,39 +324,63 @@ EVENT LISTENERS FOR ZODIAC BUTTONS
 */
 
 //click event listener for aquarius button
-_aquariusButton.addEventListener("click", function () {
-  console.log("Aquarius Button Clicked");
+_aquariusButton.addEventListener("click", function (evt) {
+  const idName = evt.target.id;
+  console.log(idName + " Clicked");
+  CalculateZodiac("aqu");
 });
-_piscesButton.addEventListener("click", function () {
-  console.log("Pisces Button Clicked");
+_piscesButton.addEventListener("click", function (evt) {
+  const idName = evt.target.id;
+  console.log(idName + " Clicked");
+  CalculateZodiac("pis");
 });
-_ariesButton.addEventListener("click", function () {
-  console.log("Aries Button Clicked");
+_ariesButton.addEventListener("click", function (evt) {
+  const idName = evt.target.id;
+  console.log(idName + " Clicked");
+  CalculateZodiac("ari");
 });
-_taurusButton.addEventListener("click", function () {
-  console.log("Taurus Button Clicked");
+_taurusButton.addEventListener("click", function (evt) {
+  const idName = evt.target.id;
+  console.log(idName + " Clicked");
+  CalculateZodiac("tau");
 });
-_geminiButton.addEventListener("click", function () {
-  console.log("Gemini Button Clicked");
+_geminiButton.addEventListener("click", function (evt) {
+  const idName = evt.target.id;
+  console.log(idName + " Clicked");
+  CalculateZodiac("gem");
 });
-_cancerButton.addEventListener("click", function () {
-  console.log("Cancer Button Clicked");
+_cancerButton.addEventListener("click", function (evt) {
+  const idName = evt.target.id;
+  console.log(idName + " Clicked");
+  CalculateZodiac("can");
 });
-_leoButton.addEventListener("click", function () {
-  console.log("Leo Button Clicked");
+_leoButton.addEventListener("click", function (evt) {
+  const idName = evt.target.id;
+  console.log(idName + " Clicked");
+  CalculateZodiac("leo");
 });
-_virgoButton.addEventListener("click", function () {
-  console.log("Virgo Button Clicked");
+_virgoButton.addEventListener("click", function (evt) {
+  const idName = evt.target.id;
+  console.log(idName + " Clicked");
+  CalculateZodiac("vir");
 });
-_libraButton.addEventListener("click", function () {
-  console.log("Libra Button Clicked");
+_libraButton.addEventListener("click", function (evt) {
+  const idName = evt.target.id;
+  console.log(idName + " Clicked");
+  CalculateZodiac("lib");
 });
-_scorpioButton.addEventListener("click", function () {
-  console.log("Scorpio Button Clicked");
+_scorpioButton.addEventListener("click", function (evt) {
+  const idName = evt.target.id;
+  console.log(idName + " Clicked");
+  CalculateZodiac("sco");
 });
-_saggitariusButton.addEventListener("click", function () {
-  console.log("Saggitarius Button Clicked");
+_saggitariusButton.addEventListener("click", function (evt) {
+  const idName = evt.target.id;
+  console.log(idName + " Clicked");
+  CalculateZodiac("sag");
 });
-_capricornButton.addEventListener("click", function () {
-  console.log("Capricorn Button Clicked");
+_capricornButton.addEventListener("click", function (evt) {
+  const idName = evt.target.id;
+  console.log(idName + " Clicked");
+  CalculateZodiac("cap");
 });
